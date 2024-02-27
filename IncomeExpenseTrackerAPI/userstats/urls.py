80a3b9d976b6649f517aba4c1535faf113043f08
benkivuva <benkivuvak5@gmail.com>
2024-02-27 18:33:06 +0300

    Add IncomeSourceSummaryStats endpoint

--- IncomeExpenseTrackerAPI/userstats/urls.py
+++ IncomeExpenseTrackerAPI/userstats/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ExpenseSummaryStats
+from .views import ExpenseSummaryStats, IncomeSourceSummaryStats
 
 urlpatterns = [
-    path('expense_category_data', ExpenseSummaryStats.as_view(), name='expense-summary'),
+    path('expense_category_data/', ExpenseSummaryStats.as_view(), name='expense-summary'),
+    path('income_source_data/', IncomeSourceSummaryStats.as_view(), name='income-source-summary'),
     # other URL patterns
 ]
